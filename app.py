5ea0472e832cc0faa71d5387efe434c2db36a6b9
ritik24scse1430089-arch <ritik.24scse1430089@gmail.com>
2026-01-19 18:07:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,11 @@ st.title("Student Pass/Fail Prediction System")
 living_area = st.number_input("Living Area", min_value=0.0, max_value=40.0, value=10.0)
 number = st.number_input("Floor Number", min_value=0.0, max_value=100.0,value=75.0)
 if st.button("Predict Result"):
- input_data = np.array([[living area, number]])
+ input_data = np.array([[living_area, number]])
  prediction = model.predict(input_data)
  if prediction[0] == 1:
   st.success("Prediction: Standard")
  else:
 
   st.error("Prediction: Posh")
+
