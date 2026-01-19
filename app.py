9706ef3514d4c2de8bc66d55f987f6e351121e0d
ritik24scse1430089-arch <ritik.24scse1430089@gmail.com>
2026-01-19 18:06:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,12 @@ import numpy as np
 model = joblib.load("house_price.pkl")
 st.title("Student Pass/Fail Prediction System")
 living_area = st.number_input("Living Area", min_value=0.0, max_value=40.0, value=10.0)
-numbers of floor = st.number_input("Floor Number", min_value=0.0, max_value=100.0,value=75.0)
+number = st.number_input("Floor Number", min_value=0.0, max_value=100.0,value=75.0)
 if st.button("Predict Result"):
- input_data = np.array([[living area, number of floor]])
+ input_data = np.array([[living area, number]])
  prediction = model.predict(input_data)
  if prediction[0] == 1:
-  st.success("Prediction: PASS")
+  st.success("Prediction: Standard")
  else:
-  st.error("Prediction: FAIL")
+
+  st.error("Prediction: Posh")
