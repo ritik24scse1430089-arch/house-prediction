4b1a9343f50e4015a5cb0c869aa21e0d741f15fa
ritik24scse1430089-arch <ritik.24scse1430089@gmail.com>
2026-01-19 18:08:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import numpy as np
 model = joblib.load("house_price.pkl")
-st.title("Student Pass/Fail Prediction System")
+st.title("house type prediction")
 living_area = st.number_input("Living Area", min_value=0.0, max_value=40.0, value=10.0)
 number = st.number_input("Floor Number", min_value=0.0, max_value=100.0,value=75.0)
 if st.button("Predict Result"):
@@ -13,4 +13,5 @@ if st.button("Predict Result"):
  else:
 
   st.error("Prediction: Posh")
+
 
